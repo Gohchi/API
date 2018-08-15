7d1ba2a3796c5cd69476a64dd30c2bd4fc49ca5c
Martín Farias <mfarias@rutatrol.com>
2018-08-15 18:30:38 -0300

    Add user info and header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import coin from './image/icon/coin.svg';
+import aerolab from './image/icon/aerolab-logo.svg';
 import './App.css';
 
+function getData(type){
+  switch(type) {
+    case 'userData': return {
+      "id": "5a03638052fd231590d04eb5",
+      "name": "John Kite",
+      "points": 2000,
+      "redeemHistory": [],
+      "createDate": new Date(1510171520852)
+    }
+    default: return null;
+  }
+}
 class App extends Component {
+  constructor(props){
+    super(props)
+    console.log(process.env);
+    
+    this.state = {
+      userData: getData('userData')
+    }
+  }
+  // componentWillMount(){
+  //   if(!process.env.API_TOKEN) return;
+  //   fetch('https://aerolab-challenge.now.sh/user/me', {  
+  //     method: 'GET',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json',
+  //       'Authorization': 'Bearer ' + process.env.API_TOKEN
+  //     }
+  //   })
+  //     .then(o => o.json())
+  //     .then(userData => {
+  //       console.log(userData)
+  //       this.setState({
+  //         userData
+  //       })
+  //     })
+  // }
   render() {
+    if(this.state.userData) {
+      let data = this.state.userData;
+      return (
+        <div className="container">
+          <div className="inlineblock">
+            <ul className="user-data">
+              <li>
+                <div className="points">
+                  <div>{data.points}</div>
+                  <img src={coin} alt="points" />
+                </div>
+              </li>
+              <li><div className="name">{data.name}</div></li>
+            </ul>
+            <img src={aerolab} className="aerolab" alt="aerolab" />    
+          </div>
+          <div className="inlineblock header">
+          </div>
+          
+        </div>
+      )
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        Loading...
       </div>
-    );
+    )
   }
 }
 
