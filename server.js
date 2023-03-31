9ee8e94dad60089d009c46221f7873f5b77e9604
Martín Farias <martin.akg1991@gmail.com>
2023-03-31 01:29:57 -0300

    updates originWhitelist

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 8081;
 console.log("WHITELIST:", process.env.CORSANYWHERE_WHITELIST);
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
+  originWhitelist: [process.env.CORSANYWHERE_WHITELIST],
   requireHeader: ['origin'],
   removeHeaders: [
     'cookie',
